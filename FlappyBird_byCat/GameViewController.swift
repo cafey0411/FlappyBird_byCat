5d4256590afb84c17999d3370306e61d4a0bcbd6
cafeyqian <qjl19870411@163.com>
2019-02-27 13:56:45 +0800

    Initial Commit

---
+++ FlappyBird_byCat/GameViewController.swift
@@ -0,0 +1,42 @@
+//
+//  GameViewController.swift
+//  FlappyBird_byCat
+//
+//  Created by cafeyqian on 2019/2/27.
+//  Copyright © 2019 cafeyqian. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .resizeFill
+        skView.presentScene(scene)
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
